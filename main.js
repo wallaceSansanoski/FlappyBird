d0f59322dc7a79ee72d7ca8b213d8f34fcb4da84
wallace <sansanoskiww@gmail.com>
2023-12-26 01:03:56 -0300

    refactoring code, fixing bug height barrier and colision bird

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ const containerGame = document.querySelector('[data-js="containerGame"]')
 const containerBarriers = document.querySelector('[data-js="containerBarriers"]')
 const scoreDisplay = document.querySelector('[data-js="score"]')
 
-let distance1 = 700
-let distance2 = 970
-let distance3 = 1250
-let distance4 = 1500
-
-
 let score = 0
 let stopGame = false
 
@@ -17,53 +11,43 @@ const createElement = (element, nameClass) => {
     return el
 }
 
-function CreateBarrier(reverse = false) {
+function CreateBarrier() {
 
     const normalBarrier = createElement('div', 'normalBarrier')
     const reverseBarrier = createElement('div', 'reverseBarrier')
 
-
     return { normalBarrier, reverseBarrier }
 }
 
-function CreateParesBarrier() {
-    const heightOfBarrier = Math.trunc(Math.random() * 400)
+
+function createParesBarrier() {
+
+    const heightOfBarrier = Math.trunc(Math.random() * 350)
+    const setHeightReverseBarrier = (500 - heightOfBarrier) - 100
 
     const paresBarries = createElement('div', 'paresBarries')
+    const border = createElement('div', 'border')
+    const border1 = createElement('div', 'border1')
 
-    const barrier = CreateBarrier()
-    const reverseBarrier = CreateBarrier()
+    const { normalBarrier } = CreateBarrier()
+    const { reverseBarrier } = CreateBarrier()
 
-    if ((500 - heightOfBarrier) - 150 < 0) {
+    reverseBarrier.appendChild(border)
+    normalBarrier.appendChild(border1)
 
-        barrier.normalBarrier.style.height = `${heightOfBarrier - Math.abs((500 - heightOfBarrier) - 150)}px`
-        reverseBarrier.reverseBarrier.style.height = `${Math.abs((500 - heightOfBarrier) - 150)}px`
+    normalBarrier.style.height = `${setHeightReverseBarrier > 370 ? heightOfBarrier + 100 : heightOfBarrier}px`
+    reverseBarrier.style.height = `${setHeightReverseBarrier > 370 ? setHeightReverseBarrier - 100 : setHeightReverseBarrier}px`
 
-        paresBarries.appendChild(barrier.normalBarrier)
-        paresBarries.appendChild(reverseBarrier.reverseBarrier)
-
-        return paresBarries
-    }
-
-    barrier.normalBarrier.style.height = `${heightOfBarrier}px`
-    reverseBarrier.reverseBarrier.style.height = `${(500 - heightOfBarrier) - 150}px`
-
-    paresBarries.appendChild(barrier.normalBarrier)
-    paresBarries.appendChild(reverseBarrier.reverseBarrier)
+    paresBarries.appendChild(normalBarrier)
+    paresBarries.appendChild(reverseBarrier)
 
     return paresBarries
 }
 
-const paresBarries1 = CreateParesBarrier()
-const paresBarries2 = CreateParesBarrier()
-const paresBarries3 = CreateParesBarrier()
-const paresBarries4 = CreateParesBarrier()
-
-const allBarries = [paresBarries1, paresBarries2, paresBarries3, paresBarries4]
-const distanceBetweenBarriers = [distance1, distance2, distance3, distance4]
+const allBarries = [createParesBarrier(), createParesBarrier(), createParesBarrier(), createParesBarrier()]
+const distanceBetweenBarriers = [700, 970, 1250, 1500]
 
 const isnertBarriers = () => {
-
     allBarries.forEach(barrier => {
         containerBarriers.appendChild(barrier)
     })
@@ -72,81 +56,78 @@ const isnertBarriers = () => {
 const insertBird = () => {
     const img = createElement('img', 'imageBird')
     img.setAttribute('data-js', 'bird')
-    img.style.bottom = `${0}px`
+    img.style.bottom = `${300}px`
     img.src = './flaap.png'
     containerGame.append(img)
 }
 
 const createScore = () => {
     const displayScore = createElement('p', 'score')
+
     displayScore.setAttribute('data-js', 'score')
     displayScore.textContent = 0
     containerGame.insertAdjacentElement('afterBegin', displayScore)
+
     return displayScore
 }
 
 isnertBarriers()
 insertBird()
-const displayScore = createScore()
 
+const displayScore = createScore()
 
 const bird = document.querySelector('[data-js="bird"]')
 
-const animation = () => {
+const isColision = (barrierLeft, heightBirdFromZero, heightBarrierBottom) => {
 
+    const birdHitTheTopBarrier = barrierLeft <= 400 && barrierLeft >= 280 && heightBirdFromZero >= (heightBarrierBottom + 48)
+    const birdHitTheBottomBarrier = barrierLeft <= 400 && barrierLeft >= 280 && heightBirdFromZero <= heightBarrierBottom
+
+    if (birdHitTheTopBarrier) {
+        clearInterval(stopIntervalID)
+        stopGame = true
+        return
+    }
+
+    if (birdHitTheBottomBarrier) {
+        clearInterval(stopIntervalID)
+        stopGame = true
+        return
+    }
+}
+
+const animation = () => {
 
     Array.from(containerBarriers.children).forEach((barrier, index) => {
 
         const heightBirdFromZero = Number(bird.style.bottom.split("px")[0])
-        const heightBarrierTop = Number(Array.from(barrier.children)[0].style.height.split("px")[0])
         const heightBarrierBottom = Number(Array.from(barrier.children)[1].style.height.split("px")[0])
         const barrierLeft = Number(barrier.style.left.split("px")[0])
+        const isBarrierInTheEndOfScreen = distanceBetweenBarriers[index] < -70
 
-        if(barrierLeft <= 400 && barrierLeft >= 280 &&  heightBirdFromZero >= (heightBarrierBottom + 110) ) {
-            clearInterval(stopIntervalID)
-            stopGame = true
-            return 
-        }
-
-        if(barrierLeft <= 400 && barrierLeft >= 280 && heightBirdFromZero <= heightBarrierBottom ) {
-            clearInterval(stopIntervalID)
-            stopGame = true
-            return 
-        }
-
-        if (heightBirdFromZero <= heightBarrierBottom && barrierLeft === 400 || heightBirdFromZero >= (heightBarrierBottom + 150) && barrierLeft === 400) {
-            clearInterval(stopIntervalID)
-            stopGame = true
-            return 
-        }
+        isColision(barrierLeft, heightBirdFromZero, heightBarrierBottom)
 
         barrier.style.left = `${distanceBetweenBarriers[index]}px`
 
         distanceBetweenBarriers[index]--
 
-        if (distanceBetweenBarriers[index] < -70) {
+        if (isBarrierInTheEndOfScreen) {
             distanceBetweenBarriers[index] = 1010
 
-            const heightOfBarrier = Math.trunc(Math.random() * 400)
+            const heightOfBarrier = Math.trunc(Math.random() * 350)
+            const setHeightReverseBarrier = (500 - heightOfBarrier) - 100
 
-            barrier.children[0].style.height = `${heightOfBarrier}px`
-
-            if ((500 - heightOfBarrier) - 150 < 0) {
-
-                barrier.children[0].style.height = `${heightOfBarrier - Math.abs((500 - heightOfBarrier) - 150)}px`
-                barrier.children[1].style.height = `${Math.abs((500 - heightOfBarrier) - 150)}px`
-            }
-
-            barrier.children[0].style.height = `${heightOfBarrier}px`
-            barrier.children[1].style.height = `${Math.abs((500 - heightOfBarrier) - 150)}px`
+            barrier.children[0].style.height = `${setHeightReverseBarrier > 370 ? heightOfBarrier + 100 : heightOfBarrier}px`
+            barrier.children[1].style.height = `${setHeightReverseBarrier > 370 ? setHeightReverseBarrier - 100 : setHeightReverseBarrier}px`
         }
     })
 }
 
 
 const countScore = () => {
-    allBarries.forEach((barrier, index) => {
-        if (Number(barrier.style.left.split("px")[0]) === 330) {
+    allBarries.forEach(barrier => {
+        const birdPassBetweenBarrier = Number(barrier.style.left.split("px")[0]) === 300
+        if (birdPassBetweenBarrier) {
             score += 100
         }
     })
@@ -156,23 +137,27 @@ const countScore = () => {
 
 containerGame.addEventListener("keydown", e => {
 
-    if(!stopGame){
-        bird.style.bottom = Number(bird.style.bottom.split('p')[0]) + 30 + 'px'
+    const heightOfBird = Number(bird.style.bottom.split('p')[0])
+
+    if (!stopGame) {
+        bird.style.bottom = heightOfBird + 30 + 'px'
     }
-
-
 })
 
 
 const stopIntervalID = setInterval(() => {
 
-    bird.style.bottom = Number(bird.style.bottom.split('p')[0]) - 2 + 'px'
+    const heightOfBird = Number(bird.style.bottom.split("px")[0])
+    const heightBirdLessThanZero = heightOfBird <= 0
+    const heightBirdMoreThan450 = heightOfBird >= 450
 
-    if (Number(bird.style.bottom.split('px')[0]) <= 0) {
+    bird.style.bottom = heightOfBird - 2 + 'px'
+
+    if (heightBirdLessThanZero) {
         bird.style.bottom = `${0}px`
     }
 
-    if (Number(bird.style.bottom.split('px')[0]) >= 450) {
+    if (heightBirdMoreThan450) {
         bird.style.bottom = `${450}px`
     }
     countScore()
